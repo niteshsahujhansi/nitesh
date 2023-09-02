1db609c903d22f2bc43efb7aaca3c7e728717407
NITESH SAHU <90353118+niteshsahujhansi@users.noreply.github.com>
2023-09-02 15:14:24 +0530

    Update setup.py

--- nitesh/setup.py
+++ nitesh/setup.py
@@ -5,6 +5,6 @@ setup(
     version='0.1',
     description='My Python calculator Package',
     packages=find_packages(),
-    # url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-    # download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+    url = 'https://github.com/niteshsahujhansi/nitesh',   # Provide either the link to your github or to your website
+    download_url = 'https://github.com/niteshsahujhansi/nitesh/archive/refs/tags/0.1.zip',    # I explain this later on
 )
